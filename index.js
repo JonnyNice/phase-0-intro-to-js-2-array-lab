d68df0ac887f06fc241a8138c4b29a4a7536e7bc
JonnyNice <116230053+JonnyNice@users.noreply.github.com>
2022-11-02 08:28:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat (name) {
+    var name_1= [...cats,name];
+    return name_1;
+}
+
+function prependCat (name) {
+    var name_2= [name, ...cats];
+    return name_2
+}
+
+function removeLastCat (name) {
+    var name_3= cats.slice(0,cats.length-1);
+    return name_3
+}
+
+function removeFirstCat (name) {
+    var name_3= cats.slice(1);
+    return name_3
+}
